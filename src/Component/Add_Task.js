6a1fd49b1c3dfe06d0e4cf013fe17f7e90214bff
NencyRana <nency@bbd.co.za>
2021-03-15 14:43:43 +0530

    Date format Change

--- src/Component/Add_Task.js
+++ src/Component/Add_Task.js
@@ -1,153 +1,166 @@
 import axios from 'axios';
-import React,{useState} from 'react'
+import moment from 'moment';
+import React, { useState } from 'react'
 import '../Validation.js'
 function Add_Task() {
-    var msg=''
+    var msg = ''
     var tempDate = new Date();
-    var date = tempDate.getDate()+'-'+ (tempDate.getMonth()+1) + '-' + tempDate.getFullYear() 
+    var date = tempDate.getDate() + '-' + (tempDate.getMonth() + 1) + '-' + tempDate.getFullYear()
     const [title, settitle] = useState()
-    const status=false
+    const status = false
     const [Task_data, setTaskData] = useState([{
-        title:'',
-        dueDate:date,
-        status:'',
-        st1:'',
-        st2:'',
-        st3:'',
-        errmsg:''
+        title: '',
+        dueDate: date,
+        status: '',
+        st1: '',
+        st2: '',
+        st3: '',
+        errmsg: ''
 
     }])
-    
-    const handle_change=(fname)=> {
-        
-        if(fname==='')
-        {
-           setTaskData({...Task_data,errmsg : 'Title is required!!'})
-            
-            
+
+    const vali_title = (fname) => {
+        var today = new Date()
+        if (fname === '') {
+            setTaskData({ ...Task_data, errmsg: 'Title is required!!' })
         }
-        else{
-            setTaskData({...Task_data,errmsg : ''})
+        else if (moment(Task_data.dueDate).format('DD-MMM-YYYY') < moment(today).format('DD-MMM-YYYY')) {
+            setTaskData({ ...Task_data, errmsg: 'Due Date must be Greater or Equal than today\'s Date' })
         }
-        
+        else {
+            setTaskData({ ...Task_data, errmsg: '' })
+        }
+
     }
-    
-    // useEffect(() => {
-        
-    //     if (btnadd_task>0) {
-    //            
-    // }, [btnadd_task])
-    const submit_data=(event)=>{
-        
-        let Subtask={
-                task1:Task_data.st1,
-                task2:Task_data.st2,
-                task3:Task_data.st3,
-            }
-            console.log(Subtask)
-            let Task_list={
-                title:Task_data.title,
-                subtask:Subtask,
-                dueDate:Task_data.dueDate
-            }
-            console.log(Task_list)
-            // Task_list=JSON.stringify(Task_list)
-            
-            axios.post('http://localhost:5000/Tasks',Task_list)
-            .then((res)=>{
+
+    const submit_data = (event) => {
+
+        let Subtask = {
+            task1: Task_data.st1,
+            task2: Task_data.st2,
+            task3: Task_data.st3,
+        }
+        console.log(Subtask)
+        let Task_list = {
+            title: Task_data.title,
+            subtask: Subtask,
+            dueDate: Task_data.dueDate
+        }
+        console.log(Task_list)
+        // Task_list=JSON.stringify(Task_list)
+
+        axios.post('http://localhost:5000/Tasks', Task_list)
+            .then((res) => {
                 console.log(res)
             })
-            .catch((err)=>{
+            .catch((err) => {
                 console.log(err)
-            })        
-            event.preventDefualt()
+            })
+        event.preventDefualt()
 
+    }
+    const vali_date = () => {
+        var today = new Date()
+
+        alert(moment(today).format('DD-MMM-YYYY'))
+        if (moment(Task_data.dueDate).format('DD-MMM-YYYY') < moment(today).format('DD-MMM-YYYY')) {
+            setTaskData({ ...Task_data, errmsg: 'Due Date must be Greater or Equal than today\'s Date' })
+        }
+        else {
+            setTaskData({ ...Task_data, errmsg: '' })
+        }
     }
     return (
         <form onSubmit={submit_data}>
-        <div className="main-add-container"> 
-            
-                <div className="container"> 
+            <div className="main-add-container">
+
+                <div className="container">
                     <div className="add-todo-header">Add To-Do List</div>
-                 
+
                     <div className="add-todo-input">
                         <div className="add-todo-input-title ">Title</div>
                         <div >
-                            <input 
+                            <input
                                 required
-                                type='text' 
-                                value={Task_data.title} 
-                                onChange={(e)=>{setTaskData({...Task_data,title:e.target.value})}}
-                                onBlur={(e)=>handle_change(e.target.value)}
+                                type='text'
+                                value={Task_data.title}
+                                onChange={(e) => { setTaskData({ ...Task_data, title: e.target.value }) }}
+                                onBlur={(e) => vali_title(e.target.value)}
                                 className="add-todo-input-data"
                             />
-                            <br/>
+                            <br />
                             <span className={Task_data.errmsg ? 'err-msg' : ''} >
-                            {
-                                Task_data.errmsg && <span>{Task_data.errmsg}</span>
-                            }
-                        </span>
+                                {
+                                    Task_data.errmsg && <span>{Task_data.errmsg}</span>
+                                }
+                            </span>
                         </div>
-                        
-                        
+
+
                     </div>
                     <div className="add-todo-input">
                         <div className="add-todo-input-title">Due Date</div>
                         <div>
-                        <input 
-                            type='date' 
-                            className="add-todo-input-data"
-                            value={Task_data.dueDate} 
-                            onChange={(e)=>setTaskData({...Task_data,dueDate:e.target.value})}/>
+                            <input
+                                type='date'
+                                className="add-todo-input-data"
+                                value={Task_data.dueDate}
+                                onBlur={(e) => vali_title(e.target.value)}
+                                onChange={(e) => setTaskData({ ...Task_data, dueDate: e.target.value })} />
+                            <br />
+                            <span className={Task_data.errmsg ? 'err-msg' : ''} >
+                                {
+                                    Task_data.errmsg && <span>{Task_data.errmsg}</span>
+                                }
+                            </span>
                         </div>
                     </div>
                     <div className="add-todo-input">
                         <div className="add-todo-input-title">Subtask 1</div>
                         <div>
-                            <textarea 
-                            className="add-todo-input-data"
-                            value={Task_data.st1} 
-                            onChange={(e)=>setTaskData({...Task_data,st1:e.target.value})}
+                            <textarea
+                                className="add-todo-input-data"
+                                value={Task_data.st1}
+                                onChange={(e) => setTaskData({ ...Task_data, st1: e.target.value })}
                             >
-                            </textarea> 
+                            </textarea>
                         </div>
-                        
+
                     </div>
                     <div className="add-todo-input">
                         <div className="add-todo-input-title">Subtask 2</div>
                         <div>
-                            <textarea 
-                            className="add-todo-input-data"
-                            value={Task_data.st2} 
-                            onChange={(e)=>setTaskData({...Task_data,st2:e.target.value})}
+                            <textarea
+                                className="add-todo-input-data"
+                                value={Task_data.st2}
+                                onChange={(e) => setTaskData({ ...Task_data, st2: e.target.value })}
                             >
-                            </textarea> 
+                            </textarea>
                         </div>
-                        
+
                     </div>
                     <div className="add-todo-input">
                         <div className="add-todo-input-title">Subtask 3</div>
                         <div>
-                            <textarea 
-                            
-                            className="add-todo-input-data"
-                            value={Task_data.st3} 
-                            onChange={(e)=>setTaskData({...Task_data,st3:e.target.value})}
-                            
+                            <textarea
+
+                                className="add-todo-input-data"
+                                value={Task_data.st3}
+                                onChange={(e) => setTaskData({ ...Task_data, st3: e.target.value })}
                             >
-                            </textarea> 
+                            </textarea>
                         </div>
-                        
+
                     </div>
-                    
+
                     <div className="add-todo-input btn1 ">
                         <button type='submit' className="btn-add" >Add</button>
-                    </div> 
-                
+                    </div>
+
                 </div>
-            
-        </div>
-    </form>
+
+            </div>
+        </form>
     )
 }
 
--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,78 +1,87 @@
 import '../App_style.css'
-import React,{useState,useEffect} from "react"
+import React, { useState, useEffect } from "react"
 import axios from "axios"
 import Moment from 'moment'
 function Home() {
     const [Task, setTask] = useState([])
+    const [flag, setflag] = useState(false)
     useEffect(() => {
-           axios.get("http://localhost:5000/Tasks")
-           .then((res)=>{
-               
-               console.log(res.data)
-               setTask(res.data)
-           }) 
-           .catch((err)=>{
-               console.log(`Error Occure${err}`)
-           })
-    },[])
-    const clear_all=()=>{
+        axios.get("http://localhost:5000/Tasks")
+            .then((res) => {
+
+                console.log(res.data)
+                setTask(res.data)
+            })
+            .catch((err) => {
+                console.log(`Error Occure${err}`)
+            })
+    }, [])
+    const clear_all = () => {
         alert('The list is empty')
         axios.delete("http://localhost:5000/Tasks")
-           .then((res)=>{
-               
-               console.log(res.data)
-               setTask(res.data)
-           }) 
-           .catch((err)=>{
-               console.log(`Error Occure${err}`)
-           })
-         
+            .then((res) => {
+
+                console.log(res.data)
+                setTask(res.data)
+            })
+            .catch((err) => {
+                console.log(`Error Occure${err}`)
+            })
+
     }
-    const check_task=()=>{
-        if (Task.length==0) {
+    const check_task = () => {
+        if (Task.length == 0) {
+
             return <div className="empty-task">------ No Task in your List ------</div>
         } else {
-            return  Task.map((task1)=>(
 
-                        
-                <div className="table-data"> 
+            return Task.map((task1) => (
+
+
+                <div className="table-data">
                     <div className="title">
-                        <div className="task-title">{task1.title} </div> 
-                        <div className="task-title1"> 
+                        <div className="task-title">
+                            {task1.title}
+                        </div>
+                        <div className="task-title1">
                             {
-                                Moment(task1.dueDate).format('DD-MM-YYYY')
+                                Moment(task1.dueDate).format('DD-MMM-YYYY')
                             }
-                        </div> 
+                        </div>
                     </div>
                     <span className="subtask-title">Subtask:</span>
-                    
-                        {
-                        
-                            <ul className="ul-list">
-                                <li>{task1.subtask.task1}</li>
-                                <li>{task1.subtask.task2}</li>
-                                <li>{task1.subtask.task3}</li>
-                            </ul>
-                    
-                        }
-                    
+
+                    {
+
+                        <ul className="ul-list">
+                            <li>{task1.subtask.task1}</li>
+                            <li>{task1.subtask.task2}</li>
+                            <li>{task1.subtask.task3}</li>
+                        </ul>
+
+                    }
+
                 </div>
-            
+
 
             ))
         }
     }
     return (
-            <div className="main">
-                <div className="remove-all">
-                   <form> <button className="remove_btn" type='submit' onClick={clear_all}>Clear List</button></form>
-                </div>
-                <div className="main-data-container"> 
-                    {
-                            check_task()     
-                    }
-                </div>
+        <div className="main">
+            <div className="add-todo-header" style={Task.length == 0 ? { display: 'none' } : {}}>To-Do List</div>
+            <div className="remove-all">
+                <form>
+                    <button
+                        className="remove_btn" style={Task.length == 0 ? { display: 'none' } : {}}
+                        type='submit' onClick={clear_all}>Clear List</button></form>
             </div>
+            <div className="main-data-container">
+                {
+                    check_task()
+                }
+            </div>
+        </div>
     )
 }
 
